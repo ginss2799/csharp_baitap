751e661927167bd70670af4f5b1f6b06ea00a452
ginss2799 <buikhanhngan27081999@gmail.com>
2023-05-14 21:54:49 +0700

    Buổi 01- Hiển thị ngày giờ
    Tạo project và làm bài tập hiển thị ngày giờ

---
+++ 01_NgayGio/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program{
+    static void Main(string [] args){
+        // De chay code, go lenh 
+        //dotnet run 
+        //Giai bai tap: hien thi ngay gio he thong
+        DateTime dt = DateTime.Now; //khai bao bien kieu ngay gio, dat ten bien dt va gan gia tri la gio hien tai bang lenh DateTime.Now
+        Console.WriteLine(dt.ToString());
+        //Nang cao: Ban hãy định dạng chuỗi xuất theo kiểu ngày--tháng-năm  giờ:phút:giây
+    }
+
+}
